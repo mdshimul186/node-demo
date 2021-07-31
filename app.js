ee5c629d7d9158161561764f5d9888450456351b
boostingbd <186mdshimul@gmail.com>
2021-07-31 20:47:53 +0600

    test

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 const http = require('http');
 
 
-// const server = http.createServer(async (req, res) => {
-//     if (req.url === '/api/demo-1' && req.method === 'GET') {
+const server = http.createServer(async (req, res) => {
+    if (req.url === '/api/demo-1' && req.method === 'GET') {
         
-//         res.writeHead(200, { 'Content-Type': 'application/json' });
-//         res.end(JSON.stringify({data:"example api 1"}));
-//     }else if (req.url === '/api/demo-2' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({data:"example api 1"}));
+    }else if (req.url === '/api/demo-2' && req.method === 'GET') {
         
-//         res.writeHead(200, { 'Content-Type': 'application/json' });
-//         res.end(JSON.stringify({data:"example api 2"}));
-//     }
-//       else {
-//         res.writeHead(404, { 'Content-Type': 'application/json' });
-//         res.end(JSON.stringify({ message: 'Route not found!' }));
-//     }
-// });
-
-// const PORT = process.env.PORT || 5010;
-// server.listen(PORT, () => console.log(`Server listening on port ${PORT}!!!`));
-
-const express = require("express");
-const app = express();
-
-app.get('/api/demo-2',(req,res)=>{
-    res.status(200).json({data:"example api 1"})
- 
-})
-app.get('/api/demo-2',(req,res)=>{
-    res.status(200).json({data:"example api 2"})
-})
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({data:"example api 2"}));
+    }
+      else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Route not found!' }));
+    }
+});
 
 const PORT = process.env.PORT || 5010;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}!!!`));
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}!!!`));
+
+// const express = require("express");
+// const app = express();
+
+// app.get('/api/demo-1',(req,res)=>{
+//     res.status(200).json({data:"example api 1"})
+ 
+// })
+// app.get('/api/demo-2',(req,res)=>{
+//     res.status(200).json({data:"example api 2"})
+// })
+
+// const PORT = process.env.PORT || 5010;
+// app.listen(PORT, () => console.log(`Server listening on port ${PORT}!!!`));
 
 
 
